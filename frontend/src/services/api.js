25994712313a5148e1cd53190aa3ae442602d0e9
HarshYadav <2k24.csaiml1b.2413391@gmail.com>
2026-02-16 18:23:55 +0530

    Debug: Log API Base URL to console

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -4,6 +4,8 @@ const API = axios.create({
   baseURL: process.env.REACT_APP_API_URL || "http://127.0.0.1:8000",
 });
 
+console.log("API Base URL:", process.env.REACT_APP_API_URL || "http://127.0.0.1:8000"); // Debugging line
+
 // Attach token automatically
 API.interceptors.request.use((req) => {
   const token = localStorage.getItem("token");
